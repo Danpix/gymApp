4bd73045b7da1a1bb1ca0417ba4093e58016370a
ObedDM <pacocobot2@gmail.com>
2025-01-26 19:37:42 -1200

    test mi branch

--- prueba.java
+++ prueba.java
@@ -2,5 +2,9 @@ public class prueba {
 
     public static void main(String[] args) {
         System.out.println("Hola");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("DDF y PJ");
+        }
     }    
 }
